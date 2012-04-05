e17298168f8cfbac7ab246487b80054e5528ec15
Brian Stone <brstone@cisco.com>
2012-04-05 14:21:11 -0700

    Added flat files. Added new example.

---
+++ example/masonry.js
@@ -0,0 +1,37 @@
+$(function () {
+    var lorem = {
+        type: 'words',
+        amount: '25',
+        ptags: false
+    };
+
+    function create () {
+        var close = $('<a>').attr('href', '#').addClass('close').text('Close'),
+            box = $('<div>').addClass('box').lorem(lorem).append(close);
+        return box;
+    }
+
+    function add () {
+        $('#container').append(create()).masonry('reload');
+    }
+
+    // Initialize masonry
+    $('#container').masonry({
+        itemSelector: '.box',
+        columnWidth: 100,
+        isAnimated: true
+    });
+
+    add();
+    add();
+    add();
+    add();
+
+    // Add new boxes
+    $('#add').click(add);
+
+    $('#container').delegate('.close', 'click', function () {
+        var $box = $(this).parent();
+        $('#container').masonry('remove', $box).masonry('reload');
+    });
+});
