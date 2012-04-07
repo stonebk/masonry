d35923e7a80c246b0a449b1acf3bfbd825e05b89
Brian Stone <brstone@cisco.com>
2012-04-06 18:14:19 -0700

    Convert example into its own plugin.

--- example/jquery.masonrywm.js
+++ example/jquery.masonrywm.js
@@ -1,17 +1,210 @@
+/**
+ * @fileoverview jquery.masonrywm.js is a jQuery plugin for window management
+ *     leveraging the jquery.masonry.js layout plugin.
+ * @author brstone@cisco.com (Brian Stone)
+ */
 (function ($) {
 
-    $.masonrywm = function () {
-        console.log('init');
+    /**
+     * Masonry window management object.
+     * @param {object} opts Instance options.
+     * @param {object} container The container element.
+     * @constructor
+     */
+    $.masonrywm = function (opts, container) {
+
+        /**
+         * The options for this instance.
+         * @type object
+         */
+        this.opts = $.extend(true, {}, $.masonrywm.defaults, opts);
+
+        /**
+         * The jQuery wrapped container element.
+         * @type object
+         */
+        this.$container = $(container);
+
+        // Initialize the object
+        this._init();
     };
 
-    $.masonrywm.prototype.add = function () {
-        console.log('add');
+    /**
+     * Initialize the masonrywm object.
+     */
+    $.masonrywm.prototype._init = function () {
+        var that = this,
+            timeout = false;
+
+        // Initialize Masonry
+        this.$container.masonry({
+            itemSelector: this.opts.itemSelector,
+            columnWidth: this.opts.grid,
+            isAnimated: this.opts.isAnimated
+        });
+
+        // Detect document window resize and adjust container windows if they
+        //     overflow.
+        $(window).resize(function () {
+            if (timeout !== false) {
+                clearTimeout(timeout);
+            }
+            timeout = setTimeout(function () {
+                that._refresh();
+            }, 20);
+        });
+
+        // Prevent other resize events from propagating to window
+        $('body').resize(function (eve) {
+            // Refresh on any resize could be a compelling effect too!
+            eve.stopPropagation();
+        });
     };
 
-    $.masonrywm.prototype.remove = function () {
-        console.log('remove');
+    /**
+     * Refresh all windows.
+     */
+    $.masonrywm.prototype._refresh = function () {
+        var that = this,
+            $windows = $(this.opts.itemSelector),
+            total = $windows.length,
+            count = 0;
+
+        $windows.each(function () {
+            that._resize($(this));
+            count += 1;
+            if (count === total) {
+                that.$container.masonry('reload');
+            }
+        });
     };
 
+    /**
+     * Add a new window to the container.
+     * @param {object} $window Window to add to container.
+     */
+    $.masonrywm.prototype.add = function ($window) {
+        var that = this;
+
+        $window
+            .resizable({
+                handles: 'all',
+                stop: function () {
+                    that._resize($(this));
+                    that.$container.masonry('reload');
+                }
+            })
+            .draggable({
+                stop: function (eve, ui) {
+                    var $boxes = ui.helper.siblings(),
+                        $box, i, offset, bottom, middle;
+
+                    for (i = 0; i < $boxes.length; i += 1) {
+                        $box = $boxes.eq(i);
+                        offset = $box.offset();
+                        middle = offset.left + ($box.width() / 2);
+                        bottom = offset.top + $box.height();
+
+                        if (eve.pageX < middle && eve.pageY < bottom) {
+                            $box.before(ui.helper);
+                            break;
+                        }
+                    }
+
+                    // Reached the end of the list - move box to end of DOM
+                    if (i === $boxes.length) {
+                        that.$container.append(ui.helper);
+                    }
+
+                    that.$container.masonry('reload');
+                }
+            });
+
+        this.$container.append($window).masonry('reload');
+    };
+
+    /**
+     * The resize function provides equivalent resizable 'containment' and
+     *     'grid' functionality with a few modifications. Containment allows
+     *     resize outside of the container vertically, but not horizontally.
+     *     Grid allows snap to functionality but only snaps on stop, allowing
+     *     smooth resizing.
+     */
+    $.masonrywm.prototype._resize = function ($box) {
+        var $container = $('#container'),
+            wbox = $box.width(),             // Width of box
+            owbox = $box.outerWidth(true),   // Width of box (including margin)
+            wmargin = owbox - wbox,          // Horizontal margin
+            wcontainer = $container.width(), // Width of container
+            hbox = $box.height(),            // Height of box
+            ohbox = $box.outerHeight(true),  // Height of box (including margin)
+            hmargin = ohbox - hbox,          // Vertical margin
+            grid = this.opts.grid,
+            remainder;
+
+        // Resize horizontally
+        if (owbox > wcontainer) {
+            $box.css('width', wcontainer - (wcontainer % grid) - wmargin);
+        } else {
+            remainder = owbox % grid;
+            if (remainder > grid / 2) {
+                // Round up
+                $box.css('width', owbox - remainder - wmargin + grid);
+            } else {
+                // Round down
+                $box.css('width', owbox - remainder - wmargin);
+            }
+        }
+
+        // Resize vertically
+        remainder = ohbox % grid;
+        if (remainder > grid / 2) {
+            // Round up
+            $box.css('height', ohbox - remainder - hmargin + grid);
+        } else {
+            // Round down
+            $box.css('height', ohbox - remainder - hmargin);
+        }
+    };
+
+    /**
+     * Remove a window from the container.
+     * @param {object} $window The window to remove from the container.
+     */
+    $.masonrywm.prototype.remove = function ($window) {
+        this.$container.masonry('remove', $window).masonry('reload');
+    };
+
+    /**
+     * Default settings.
+     * @static
+     * @class
+     */
+    $.masonrywm.defaults = {
+
+        /**
+         * Grid size used for masonry columns and snap to positions.
+         * @type number
+         */
+        grid: 100,
+
+        /**
+         * Enables animation on layout changes.
+         * @type boolean
+         */
+        isAnimated: true,
+
+        /**
+         * Filters item elements to selector. If not set, Masonry defaults to
+         *     using the child elements of the container.
+         * @type string
+         */
+        itemSelector: '.box-wrap'
+    };
+
+    /**
+     * Add masonry window management as a plugin to jQuery.
+     */
     $.fn.masonrywm = function (method) {
         if (typeof method === 'string') {
             var args = Array.prototype.slice.call(arguments, 1);
@@ -31,7 +224,7 @@
             this.each(function () {
                 var instance = $.data(this, 'masonrywm');
                 if (!instance) {
-                    $.data(this, 'masonrywm', new $.masonrywm());
+                    $.data(this, 'masonrywm', new $.masonrywm(method, this));
                 }
             });
         }
--- example/masonry.js
+++ example/masonry.js
@@ -1,12 +1,11 @@
 $(function () {
-    var lorem = {
-        type: 'words',
-        amount: '25',
-        ptags: false
-    }, GRID = 100;
-
-    function create () {
-        var close = $('<a>')
+    function add () {
+        var lorem = {
+                type: 'words',
+                amount: '25',
+                ptags: false
+            },
+            close = $('<a>')
                 .attr('href', '#')
                 .addClass('close')
                 .text('Close'),
@@ -18,143 +17,27 @@ $(function () {
                 .addClass('box-wrap')
                 .append($box);
 
-        // Make box resizable
-        $wrap
-            .resizable({
-                handles: 'all',
-                stop: function () {
-                    resize($(this));
-                    $('#container').masonry('reload');
-                }
-            })
-            .draggable({
-                stop: function (eve, ui) {
-                    var $boxes = ui.helper.siblings(),
-                        $box, i, offset, bottom, middle;
-
-                    for (i = 0; i < $boxes.length; i += 1) {
-                        $box = $boxes.eq(i);
-                        offset = $box.offset();
-                        middle = offset.left + ($box.width() / 2);
-                        bottom = offset.top + $box.height();
-
-                        if (eve.pageX < middle && eve.pageY < bottom) {
-                            $box.before(ui.helper);
-                            break;
-                        }
-                    }
-
-                    // Reached the end of the list - move box to end of DOM
-                    if (i === $boxes.length) {
-                        $('#container').append(ui.helper);
-                    }
-
-                    $('#container').masonry('reload');
-                }
-            });
-
-        return $wrap;
+        $('#container').masonrywm('add', $wrap);
     }
 
-    /**
-     * The resize function provides equivalent resizable 'containment' and
-     *     'grid' functionality with a few modifications. Containment allows
-     *     resize outside of the container vertically, but not horizontally.
-     *     Grid allows snap to functionality but only snaps on stop, allowing
-     *     smooth resizing.
-     */
-    function resize ($box) {
-        var $container = $('#container'),
-            wbox = $box.width(),             // Width of box
-            owbox = $box.outerWidth(true),   // Width of box (including margin)
-            wmargin = owbox - wbox,          // Horizontal margin
-            wcontainer = $container.width(), // Width of container
-            hbox = $box.height(),            // Height of box
-            ohbox = $box.outerHeight(true),  // Height of box (including margin)
-            hmargin = ohbox - hbox,          // Vertical margin
-            remainder;
-
-        // Resize horizontally
-        if (owbox > wcontainer) {
-            $box.css('width', wcontainer - (wcontainer % GRID) - wmargin);
-        } else {
-            remainder = owbox % GRID;
-            if (remainder > GRID / 2) {
-                // Round up
-                $box.css('width', owbox - remainder - wmargin + GRID);
-            } else {
-                // Round down
-                $box.css('width', owbox - remainder - wmargin);
-            }
-        }
-
-        // Resize vertically
-        remainder = ohbox % GRID;
-        if (remainder > GRID / 2) {
-            // Round up
-            $box.css('height', ohbox - remainder - hmargin + GRID);
-        } else {
-            // Round down
-            $box.css('height', ohbox - remainder - hmargin);
-        }
-    }
-
-    function add () {
-        $('#container').append(create()).masonry('reload');
-    }
-
-    // Initialize masonry
-    $('#container').masonry({
-        itemSelector: '.box-wrap',
-        columnWidth: 100,
-        isAnimated: true
-    });
-
-    add();
-    add();
-    add();
-    add();
-
-    function refresh () {
-        var $boxes = $('.box-wrap'),
-            total = $boxes.length,
-            count = 0;
-        $('.box-wrap').each(function () {
-            resize($(this));
-            count += 1;
-            if (count === total) {
-                $('#container').masonry('reload');
-            }
-        });
-    }
-
-    // Prevent other resize events from propagating to parent
-    $('body').resize(function (eve) {
-        // Refresh on any resize could be a compelling effect too!
-        eve.stopPropagation();
-    });
-
-    // Detect to window resizes and the fix the size of boxes if they are too
-    // big for the container
-    var to = false;
-    $(window).resize(function () {
-        if (to !== false) {
-            clearTimeout(to);
-        }
-        to = setTimeout(function () {
-            refresh();
-        }, 20);
-    });
-
-    // Add new boxes
+    // Handler to add new boxes
     $('#add').click(function () {
         add();
         return false;
     });
 
+    // Initialize masonry window management
+    $('#container').masonrywm({ /* Settings */ });
+
+    // Add some initial boxes
+    add();
+    add();
+    add();
+    add();
+
     $('#container').delegate('.close', 'click', function () {
         var $box = $(this).closest('.box-wrap');
-        $('#container').masonry('remove', $box).masonry('reload');
+        $('#container').masonrywm('remove', $box);
         return false;
     });
 });
